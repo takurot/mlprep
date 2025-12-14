f1683127353f316b1009c77bb994924efc29449e
Takuro Tsujikawa <takuro.tsujikawa@gmail.com>
2025-12-14 18:45:17 +0900

    fix(python): suppress ruff lint errors in __init__.py

--- python/mlprep/__init__.py
+++ python/mlprep/__init__.py
@@ -1,5 +1,5 @@
-from .mlprep import *
+from .mlprep import *  # noqa: F403
 
-__doc__ = mlprep.__doc__
-if hasattr(mlprep, "__all__"):
-    __all__ = mlprep.__all__
+__doc__ = mlprep.__doc__  # noqa: F405
+if hasattr(mlprep, "__all__"):  # noqa: F405
+    __all__ = mlprep.__all__  # noqa: F405
